4a0342ee7e7e5f6f45f93882398ca82edb62e76b
Velko <wlsgurrla716@naver.com>
2026-01-03 03:30:11 +0900

    ✨[Feat]#1 - Mock 데이터 수정

--- SMUMeal/Core/Repository/Sources/Mock/MockMealRepository.swift
+++ SMUMeal/Core/Repository/Sources/Mock/MockMealRepository.swift
@@ -8,22 +8,23 @@
 #if DEBUG
 import Foundation
 import Network
+import Domain
 
-//public struct MockMealRepository: MealRepositoryProtocol {
-//    public init() {}
-//
-//    public func fetchMeal(targetDate: String) async throws -> CafeteriaMenu {
-//        CafeteriaMenu(
-//            breakfastMenu: "토스트, 우유, 사과",
-//            lunchMenu: "김치찌개, 밥, 계란말이",
-//            dinnerMenu: "돈까스, 샐러드, 미소국",
-//            breakfastLikeCount: 10,
-//            lunchLikeCount: 25,
-//            dinnerLikeCount: 15,
-//            breakfastDislikeCount: 2,
-//            lunchDislikeCount: 5,
-//            dinnerDislikeCount: 3
-//        )
-//    }
-//}
+public struct MockMealRepository: MealRepositoryProtocol {
+    public init() {}
+
+    public func fetchMeal(cafeteriaType: CafeteriaType, targetDate: String) async throws -> CafeteriaMenu {
+        CafeteriaMenu(
+            breakfastMenu: "토스트, 우유, 사과",
+            lunchMenu: "김치찌개, 밥, 계란말이",
+            dinnerMenu: "돈까스, 샐러드, 미소국",
+            breakfastLikeCount: 10,
+            lunchLikeCount: 25,
+            dinnerLikeCount: 15,
+            breakfastDislikeCount: 2,
+            lunchDislikeCount: 5,
+            dinnerDislikeCount: 3
+        )
+    }
+}
 #endif
