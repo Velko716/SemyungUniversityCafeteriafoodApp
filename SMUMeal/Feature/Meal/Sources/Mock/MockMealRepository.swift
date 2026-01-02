d868e4da3c1eabc7cea733eddef0ddefe3bf64c2
Velko <wlsgurrla716@naver.com>
2026-01-03 03:28:30 +0900

    ✨[Feat]#1 - 식당 정보 Mock 데이터 구현

---
+++ SMUMeal/Feature/Meal/Sources/Mock/MockMealRepository.swift
@@ -0,0 +1,31 @@
+//
+//  MockMealRepository.swift
+//  Meal
+//
+//  Created by 김진혁 on 1/3/26.
+//
+
+#if DEBUG
+import Foundation
+import Network
+import Domain
+import Repository
+
+struct MockMealRepository: MealRepositoryProtocol {
+    init() {}
+
+    func fetchMeal(cafeteriaType: CafeteriaType, targetDate: String) async throws -> CafeteriaMenu {
+        CafeteriaMenu(
+            breakfastMenu: "토스트, 우유, 사과",
+            lunchMenu: "김치찌개, 밥, 계란말이",
+            dinnerMenu: "돈까스, 샐러드, 미소국",
+            breakfastLikeCount: 10,
+            lunchLikeCount: 25,
+            dinnerLikeCount: 15,
+            breakfastDislikeCount: 2,
+            lunchDislikeCount: 5,
+            dinnerDislikeCount: 3
+        )
+    }
+}
+#endif
