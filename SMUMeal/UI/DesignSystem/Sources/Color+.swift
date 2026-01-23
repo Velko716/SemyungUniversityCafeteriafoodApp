3dceadd63db1ccd91591b040abae8aa4c75f447c
Velko <wlsgurrla716@naver.com>
2026-01-23 17:27:08 +0900

    💄[Design]#1 -컬러 에셋 추가

--- SMUMeal/UI/DesignSystem/Sources/Color+.swift
+++ SMUMeal/UI/DesignSystem/Sources/Color+.swift
@@ -8,5 +8,31 @@
 import SwiftUI
 
 public extension Color {
-    static let black01 = DesignSystemAsset.black01.swiftUIColor
+    // Black
+    static let black01 = DesignSystemAsset.Black.black01.swiftUIColor
+
+    // Blue
+    static let blue01 = DesignSystemAsset.blue01.swiftUIColor
+    static let blue02 = DesignSystemAsset.blue02.swiftUIColor
+
+    // Gray
+    static let gray01 = DesignSystemAsset.gray01.swiftUIColor
+    static let gray02 = DesignSystemAsset.gray02.swiftUIColor
+    static let gray03 = DesignSystemAsset.gray03.swiftUIColor
+
+    // Navy
+    static let navy01 = DesignSystemAsset.navy01.swiftUIColor
+
+    // Pink
+    static let pink01 = DesignSystemAsset.pink01.swiftUIColor
+
+    // Red
+    static let red01 = DesignSystemAsset.red01.swiftUIColor
+    static let red02 = DesignSystemAsset.red02.swiftUIColor
+
+    // White
+    static let white01 = DesignSystemAsset.white01.swiftUIColor
+
+    // Yellow
+    static let yellow01 = DesignSystemAsset.yellow01.swiftUIColor
 }
